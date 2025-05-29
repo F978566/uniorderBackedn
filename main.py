dbb9fb15f07f5923b72735d34656fad0a68318df
F978566 <77454354+F978566@users.noreply.github.com>
2025-05-30 00:45:25 +0300

    add migrartions

--- main.py
+++ main.py
@@ -38,4 +38,5 @@ async def log_requests(request: Request, call_next):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="10.162.192.114", port=8000)
+    # uvicorn.run(app, host="10.162.192.114", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
